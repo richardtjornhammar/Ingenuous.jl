11a954cf8c9bea5d6146973a506ecbb34cbd20ad
Richard Tjörnhammar <richard.tjornhammar@gmail.com>
2021-02-18 14:14:34 +0100

    proj++

--- src/projection.jl
+++ src/projection.jl
@@ -1,33 +1,30 @@
-module clustering
+module projection
 
 using LinearAlgebra
 
-export function distance_matrix_to_absolute_coordinates
+export distance_matrix_to_absolute_coordinates
 
-function distance_matrix_to_absolute_coordinates ( D::Array{Float64,2},
-	 					 bSquared::Bool=false ,
-						 n_dimensions::Int64=2 ):
+function distance_matrix_to_absolute_coordinates( D::Array{Float64,2} , bSquared::Bool=false , n_dimensions::Int64=2 )
     # Same function can be found in my impetuous-gfa repo here:
     # https://github.com/richardtjornhammar/impetuous/blob/master/src/impetuous/clustering.py
     # around line 121 (same name)
     if not bSquared
-        D = D**2.
+        D = D.^2.
     end
-    N , M = size(D)
+    N,M = size(D)
     DIM = n_dimensions
-    DIJ = D*0.
-    for i in 1:M :
-        for j in 1:M :
+    DIJ = D.*0.
+    for i in 1:M
+        for j in 1:M
             DIJ[i,j] = 0.5* (D[i,end]+D[j,end]-D[i,j])
+	end
+    end
     D = DIJ
-    println(D)
-    """
-    U,S,Vt = np.linalg.svd ( D , full_matrices = True )
-    S[DIM:] *= 0.
-    Z = np.diag(S**0.5)[:,:DIM]
-    xr = np.dot( Z.T,Vt )
+    U,S,Vt = svd( D )
+    S[DIM+1:end] *= 0.
+    Z  = diagm(S.^0.5)[:,1:DIM]
+    xr = Vt*Z
     return ( xr )
-    """
 end
 
 end
